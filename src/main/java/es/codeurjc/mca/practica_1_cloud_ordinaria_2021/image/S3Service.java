0784fa8df835c4ad65ffdc6e8db86e01b0afd91c
juablaz <jc-blazquez@hotmail.com>
2021-03-06 20:23:24 +0100

    jar

--- src/main/java/es/codeurjc/mca/practica_1_cloud_ordinaria_2021/image/S3Service.java
+++ src/main/java/es/codeurjc/mca/practica_1_cloud_ordinaria_2021/image/S3Service.java
@@ -3,12 +3,8 @@ package es.codeurjc.mca.practica_1_cloud_ordinaria_2021.image;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.AmazonS3Exception;
-import com.amazonaws.services.s3.model.Bucket;
-import com.amazonaws.services.s3.model.S3ObjectSummary;
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -18,13 +14,13 @@ import org.springframework.web.multipart.MultipartFile;
 @Profile("production")
 public class S3Service {
 
-    @Value("${amazon.s3.region:'us-east-1'}")
-    private String REGION;
+    private String region;
 
     public static AmazonS3 s3;
 
-    public S3Service() {
-        s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.fromName(REGION)).build();
+    public S3Service(@Value("${amazon.s3.region:'us-east-1'}") String region) {
+        this.region = region;
+        s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.fromName(this.region)).build();
     }
 
     public void createBucketIfNotExists(String bucketName) {
